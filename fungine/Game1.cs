52d6755216fa8661df577d7b0656c4fd0fb087a2
el_sime <40828293+el-sime@users.noreply.github.com>
2024-06-06 14:18:11 +0200

    WIP adding knight

--- fungine/Game1.cs
+++ fungine/Game1.cs
@@ -9,6 +9,7 @@ public class Game1 : Game
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
 
+    Texture2D knightIdle;
     public Game1()
     {
         _graphics = new GraphicsDeviceManager(this);
@@ -26,8 +27,8 @@ public class Game1 : Game
     protected override void LoadContent()
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
-
         // TODO: use this.Content to load your game content here
+        knightIdle = Content.Load<Texture2D>("knight_idle_spritesheet.png");
     }
 
     protected override void Update(GameTime gameTime)
@@ -45,7 +46,9 @@ public class Game1 : Game
         GraphicsDevice.Clear(Color.CornflowerBlue);
 
         // TODO: Add your drawing code here
-
+        _spriteBatch.Begin();
+        _spriteBatch.Draw(knightIdle, new Vector2(20, 20), Color.White);
+        _spriteBatch.End();
         base.Draw(gameTime);
     }
 }
